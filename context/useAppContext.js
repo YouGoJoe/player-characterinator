96fefdd61cbd3c188c096af3978729bfa6e12060
Joe Staller <joestaller23@gmail.com>
2021-12-30 10:07:54 -0500

    classes working well

--- context/useAppContext.js
+++ context/useAppContext.js
@@ -1,11 +1,13 @@
 import React from "react";
 import useReducer from "./useReducer";
+import { orderBy, isEqual } from "lodash";
 import rollStats, {
   weakThreshold,
   isWeakChar,
   boringThreshold,
   isBoringChar,
 } from "./rollStats";
+import pickOne from "../utils/pickOne";
 
 const showMore = false;
 const allowWeak = false;
@@ -25,22 +27,49 @@ const WIZARD = "Wizard";
 const WARLOCK = "Warlock";
 
 const classes = [
-  BARBARIAN,
-  BARD,
-  CLERIC,
-  DRUID,
-  FIGHTER,
-  MONK,
-  PALADIN,
-  RANGER,
-  ROGUE,
-  SORCERER,
-  WIZARD,
-  WARLOCK,
+  BARBARIAN, // str, con
+  BARD, // cha, dex
+  CLERIC, // wis, str or con
+  DRUID, // wis, con
+  FIGHTER, // str or dex, con
+  MONK, // dex, wis
+  PALADIN, // str, cha
+  RANGER, // dex, wis
+  ROGUE, // dex
+  SORCERER, // cha, con
+  WIZARD, // int, con
+  WARLOCK, // cha, con
 ];
 
 const pickClass = (stats) => {
-  return classes[Math.floor(Math.random() * classes.length)];
+  const orderedStats = orderBy(
+    Object.entries(stats).map(([name, value]) => ({ name, value })),
+    "value",
+    "desc"
+  );
+
+  // stats heavily bias towards one class
+  const topTwo = [orderedStats[0].name, orderedStats[1].name].sort();
+
+  if (isEqual(topTwo, ["dex", "wis"])) return MONK;
+  if (isEqual(topTwo, ["cha", "str"])) return PALADIN;
+  if (isEqual(topTwo, ["cha", "dex"])) return BARD;
+  if (isEqual(topTwo, ["str", "wis"])) return CLERIC;
+
+  // bias class based on highest stat
+  let highestStat = orderedStats[0];
+
+  // con doesn't bias you to a class well, so we'll take the next highest as your primary stat
+  if (highestStat.name === "con") highestStat = orderedStats[1];
+
+  if (highestStat.name === "str") return pickOne([FIGHTER, BARBARIAN, PALADIN]);
+  if (highestStat.name === "dex") return pickOne([ROGUE, FIGHTER, RANGER]);
+  if (highestStat.name === "int") return WIZARD;
+  if (highestStat.name === "wis") return pickOne([DRUID, CLERIC]);
+  if (highestStat.name === "cha") return pickOne([BARD, SORCERER, WARLOCK]);
+
+  // randomize
+  return pickOne(classes);
 };
 
 const useAppContext = (rolledStats = rollStats({ allowBoring, allowWeak })) => {
