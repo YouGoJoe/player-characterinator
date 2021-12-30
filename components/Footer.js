8014bc9af9a392af11f285358f92c57f1d522bd4
Joe Staller <joestaller23@gmail.com>
2021-12-30 14:24:19 -0500

    adds Home button to Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,28 @@
-import styles from './Footer.module.css'
+import HomeIcon from "@mui/icons-material/Home";
+import Link from "next/link";
+import { Button } from "@mui/material";
+
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <Link href="/">
+          <Button variant="text" startIcon={<HomeIcon />}>
+            Home
+          </Button>
+        </Link>
+        <span>
+          Made with{" "}
+          <img
+            src="/netliheart.svg"
+            alt="Netlify Logo"
+            className={styles.logo}
+          />{" "}
+          for you
+        </span>
       </footer>
     </>
-  )
+  );
 }
